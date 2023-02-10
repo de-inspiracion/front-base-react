f8cae22e6630909067c0abe4836bbe368ed5e38b
RODRIGOH2GARRIDO <imrodrigohgarrido@gmail.com>
2023-02-10 12:33:55 -0300

    cursos en progreso en home

--- src/components/cardSlice/cardSlice.tsx
+++ src/components/cardSlice/cardSlice.tsx
@@ -2,10 +2,18 @@ import { useState, useEffect } from "react";
 import "./cardSlice.css";
 import CardV from "../cards/card";
 import { useSwipeable } from "react-swipeable";
-
+import { Card, Space, Typography } from "antd";
+const { Meta } = Card
 import services from "./services/http";
-
+import { useSelector } from "react-redux";
+import CourseInProgressModal from "./components/CourseInProgressModal";
 function CardSlide({ title, id, description, courses, source }: any) {
+  const [abrirModal, setAbrirModal] = useState(false);
+  const [dataModal, setDataModal] = useState({});
+  const handleCloseModal = () => {
+    setAbrirModal(false);
+  };
+  const userInfo = useSelector((estado:any) => estado.userInfo)
   const [items, setIems] = useState([]);
   useEffect(() => {
     const getData = async () => {
@@ -14,6 +22,9 @@ function CardSlide({ title, id, description, courses, source }: any) {
     };
     if(source === 'Ruta'){
       setIems(courses)
+    }
+    else if( source === 'En Progreso'){
+      console.log('en progreso')
     }
     else{
       getData()
@@ -51,31 +62,78 @@ function CardSlide({ title, id, description, courses, source }: any) {
 
   return (
     <>
-      <div className="movieRow" {...handlers}>
-        <h2> {title} </h2>
-        <div className="movieRow--left" onClick={handleLeftArrow}>
-          <img src="https://img.icons8.com/ios-glyphs/50/FFFFFF/chevron-left.png" />
-        </div>
-        <div className="movieRow--right" onClick={handleRightArrow}>
-          <img src="https://img.icons8.com/ios-glyphs/50/FFFFFF/chevron-right.png" />
-        </div>
+      { source == "En Progreso" ?
+          <div className="movieRow" {...handlers}>
+          <h2> {title} </h2>
+          <div className="movieRow--left" onClick={handleLeftArrow}>
+            <img src="https://img.icons8.com/ios-glyphs/50/FFFFFF/chevron-left.png" />
+          </div>
+          <div className="movieRow--right" onClick={handleRightArrow}>
+            <img src="https://img.icons8.com/ios-glyphs/50/FFFFFF/chevron-right.png" />
+          </div>
 
-        <div className="movieRow--listarea">
-          <div
-            className="movieRow--list"
-            style={{
-              marginLeft: scrollX,
-            }}
-          >
-            {
-              items.length > 0 && items.map((item,key) => {
-                // console.log(item)
-                return <CardV key={key} itemData = {item} Image={item.cover} index={key}/>
-              })
-            }
+          <div className="movieRow--listarea">
+            <div
+              className="movieRow--list"
+              style={{
+                marginLeft: scrollX,
+              }}
+            >
+              {userInfo.inprogress.map((curso: any) => {
+                return (
+                  <Card
+                    style={{ width: 240, cursor: "pointer" }}
+                    cover={<img alt="cover" src={curso.course.cover} />}
+                    onClick={() => {
+                      setDataModal(curso);
+                      setAbrirModal(true);
+                    }}
+                  >
+                    <Meta
+                      style={{ textAlign: "center" }}
+                      title={curso.course.name}
+                    />
+                  </Card>
+                );
+              })}
+            </div>
           </div>
         </div>
-      </div>
+        :
+        <div className="movieRow" {...handlers}>
+          <h2> {title} </h2>
+          <div className="movieRow--left" onClick={handleLeftArrow}>
+            <img src="https://img.icons8.com/ios-glyphs/50/FFFFFF/chevron-left.png" />
+          </div>
+          <div className="movieRow--right" onClick={handleRightArrow}>
+            <img src="https://img.icons8.com/ios-glyphs/50/FFFFFF/chevron-right.png" />
+          </div>
+
+          <div className="movieRow--listarea">
+            <div
+              className="movieRow--list"
+              style={{
+                marginLeft: scrollX,
+              }}
+            >
+              {
+                items.length > 0 && items.map((item,key) => {
+                  // console.log(item)
+                  return <CardV key={key} itemData = {item} Image={item.cover} index={key}/>
+                })
+              }
+            </div>
+          </div>
+        </div>
+      }
+      {abrirModal && (
+          <CourseInProgressModal
+            Data={dataModal}
+            Open={abrirModal}
+            Cerrar={handleCloseModal}
+          />
+        )}
+
     </>
   );
 }
--- src/pages/home/components/coursesInProgress/CoursesInProgress.tsx
+++ src/pages/home/components/coursesInProgress/CoursesInProgress.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Card, Row } from "antd";
+import { Card, Row, Space, Typography } from "antd";
+const { Meta } = Card
 import "./coursesInProgress.css";
 import { getListInProgress } from "../../services/home.service";
 import { useSelector, useDispatch } from "react-redux";
 import { newDataUser } from "../../../../store/user/userData";
 import CardSlice from "../../../../components/cardSlice/cardSlice";
 import services from "./services/http";
-
+import CourseInProgressModal from "./components/CourseInProgressModal";
 const inProfressList = [
   { title: "curso 1", key: 0 },
   { title: "curso 2", key: 1 },
@@ -17,8 +18,7 @@ const coursesInProgress = getListInProgress().then((data) => {
 });
 
 const CoursesInProgress: React.FC = () => {
-  const count = useSelector((state: any) => state.userInfo);
-  // console.log(count)
+  const userInfo = useSelector((state: any) => state.userInfo);
   const dispatch = useDispatch();
   const changeValue = () => {
     dispatch(newDataUser("alejandro"));
@@ -39,6 +39,9 @@ const CoursesInProgress: React.FC = () => {
         gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
         style={{ overflow: "hidden" }}
       >
+        {userInfo.inprogress.length > 0 && 
+          <CardSlice title="Cursos en Progreso" id="progreso" source="En Progreso" description="Cursos en Progreso"/>
+        }
         {categories.length > 0 &&
           categories.map((categoria: any) => {
             return (
