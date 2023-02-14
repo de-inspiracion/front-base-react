962e1d9fb83e7a3c963ef8165dd4a6c85e2eb535
alejandro oñate <alejandro.onate.c@gmail.com>
2023-02-14 02:54:36 -0300

    crud files

--- src/pages/admin-virgo/pages/editCourses/components/videoEditor/videoEditor.tsx
+++ src/pages/admin-virgo/pages/editCourses/components/videoEditor/videoEditor.tsx
@@ -1,125 +1,139 @@
-import { Button, Descriptions, List } from "antd";
+import { Button, Descriptions, List, Upload } from "antd";
 import Paragraph from "antd/es/typography/Paragraph";
 import React, { useState } from "react";
 import { ConfigProvider, theme } from "antd";
-import { Card, Space, Grid, Row, Col } from 'antd';
+import { Card, Space, Grid, Row, Col } from "antd";
 import { Divider, Radio, Typography, Popover, Modal } from "antd";
-import { PlusCircleOutlined, DeleteOutlined, CompassOutlined } from '@ant-design/icons';
-import services from '../../../../../../services/http'
+import {
+  PlusCircleOutlined,
+  DeleteOutlined,
+  CompassOutlined,
+  SettingOutlined,
+  EditOutlined,
+  EllipsisOutlined,
+  QuestionOutlined,
+  FilePdfOutlined,
+  UploadOutlined,
+} from "@ant-design/icons";
+import services from "../../../../../../services/http";
 import NewVideo from "./NewVideo";
 import { message } from "antd";
-interface customProps{
-  videos:any,
-  id:any
+import type { UploadProps } from "antd";
+
+const base_url = import.meta.env.VITE_BASE_URL;
+interface customProps {
+  videos: any;
+  id: any;
 }
 
-export const VideoEditor = (props:customProps) => {
-
+export const VideoEditor = (props: customProps) => {
   const { defaultAlgorithm, darkAlgorithm } = theme;
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
+  const [openModalFiles, setOpenModalFiles] = useState(false);
   const [messageApi, contextHolder] = message.useMessage();
-  const [currentVideoSelected, setcurrentVideoSelected] = useState('');
+  const [currentVideoSelected, setcurrentVideoSelected] = useState("");
+  const [listFiles, setListFiles] = useState([] as any);
   const questionsDefault = [
     {
-        "question": "una pregunta?",
-        "number": 1,
-        "options": [
-            {
-                "option": "opcion 1",
-                "number": 1,
-                "correct": false
-            },
-            {
-                "option": "opcion 2",
-                "number": 2,
-                "correct": true
-            },
-            {
-                "option": "opcion 3",
-                "number": 3,
-                "correct": false
-            }
-        ]
+      question: "una pregunta?",
+      number: 1,
+      options: [
+        {
+          option: "opcion 1",
+          number: 1,
+          correct: false,
+        },
+        {
+          option: "opcion 2",
+          number: 2,
+          correct: true,
+        },
+        {
+          option: "opcion 3",
+          number: 3,
+          correct: false,
+        },
+      ],
     },
     {
-      "question": "una pregunta2 ?",
-      "number": 2,
-      "options": [
-          {
-              "option": "opcion 10",
-              "number": 1,
-              "correct": false
-          },
-          {
-              "option": "opcion 2",
-              "number": 2,
-              "correct": true
-          },
-          {
-              "option": "opcion 3",
-              "number": 3,
-              "correct": false
-          }
-      ]
-  },
-  {
-    "question": "una pregunta3 ?",
-    "number": 3,
-    "options": [
+      question: "una pregunta2 ?",
+      number: 2,
+      options: [
         {
-            "option": "opcion 1",
-            "number": 1,
-            "correct": false
+          option: "opcion 10",
+          number: 1,
+          correct: false,
         },
         {
-            "option": "opcion 2",
-            "number": 2,
-            "correct": true
+          option: "opcion 2",
+          number: 2,
+          correct: true,
         },
         {
-            "option": "opcion 3",
-            "number": 3,
-            "correct": false
-        }
-    ]
-}
-]
+          option: "opcion 3",
+          number: 3,
+          correct: false,
+        },
+      ],
+    },
+    {
+      question: "una pregunta3 ?",
+      number: 3,
+      options: [
+        {
+          option: "opcion 1",
+          number: 1,
+          correct: false,
+        },
+        {
+          option: "opcion 2",
+          number: 2,
+          correct: true,
+        },
+        {
+          option: "opcion 3",
+          number: 3,
+          correct: false,
+        },
+      ],
+    },
+  ];
 
   let questionForVideo: any[] = [];
   props.videos.forEach((questionVideo: any) => {
-    return questionForVideo.push(questionsDefault)
-  })
+    return questionForVideo.push(questionsDefault);
+  });
 
-  const [questions, setQuestion] = useState(questionsDefault)
+  const [questions, setQuestion] = useState(questionsDefault);
   const editVideoInfo = async (id: any, body: any) => {
-    await services.editVideoInfo(id, body)
-  }
+    await services.editVideoInfo(id, body);
+  };
 
   const editQuestionTitle = async (text: string, indexQuestion: number) => {
-    questions[indexQuestion].question = text;;
-    setQuestion([...questions])
-  }
+    questions[indexQuestion].question = text;
+    setQuestion([...questions]);
+  };
 
   const editOptionQuestion = async (
     text: string,
     indexQuestion: number,
     indexOption: number
-    ) => {
+  ) => {
     questions[indexQuestion].options[indexOption].option = text;
-    setQuestion([...questions])
-  }
+    setQuestion([...questions]);
+  };
 
   const editCorrectOptionQuestion = async (
     indexQuestion: number,
     indexOption: any
-    ) => {
-    indexOption = Number(indexOption)
+  ) => {
+    indexOption = Number(indexOption);
     questions[indexQuestion].options.forEach((opt) => {
-      opt.correct = false
-    })
+      opt.correct = false;
+    });
     questions[indexQuestion].options[indexOption].correct = true;
-    setQuestion([...questions])
-  }
+    setQuestion([...questions]);
+  };
 
   const valueOptionSelected = (options: any[]) => {
     let indexOptionSeleceted = 0;
@@ -127,54 +141,104 @@ export const VideoEditor = (props:customProps) => {
       if (option.correct === true) {
         indexOptionSeleceted = idx;
       }
-    })
+    });
     return indexOptionSeleceted;
-  }
+  };
 
-  const updateQuestions = async ()  => {
+  const updateQuestions = async () => {
     try {
-      await services.addQuestions(currentVideoSelected, questions)
+      await services.addQuestions(currentVideoSelected, questions);
       messageApi.open({
-        type: 'success',
-        content: 'Se realizó la acción de forma correcta.',
+        type: "success",
+        content: "Se realizó la acción de forma correcta.",
       });
-      setOpen(false)
+      setOpen(false);
     } catch (error) {
       messageApi.open({
-        type: 'error',
-        content: 'error en la actualización.',
+        type: "error",
+        content: "error en la actualización.",
       });
     }
-   
-  }
+  };
   const getQuestionFromVideo = async (idVideo: string, index: number) => {
     let res = await services.getQuestions(idVideo);
-    if(!res.data.payload) {
-      const questionVideo = questionForVideo[index]
-      setQuestion([...questionVideo])
+    if (!res.data.payload) {
+      const questionVideo = questionForVideo[index];
+      setQuestion([...questionVideo]);
       return;
     }
 
     const data = res.data.payload.questions;
-    setQuestion([...data])
-    
-  }
+    setQuestion([...data]);
+  };
 
   const openModal = (idVideo: any, index: any) => {
-    setcurrentVideoSelected(idVideo)
+    setcurrentVideoSelected(idVideo);
     getQuestionFromVideo(idVideo, index);
     setOpen(true);
-  }
+  };
+
+  const handleOpenModalFiles = (idVideo: any, index: any) => {
+    setcurrentVideoSelected(idVideo);
+    getFilesVideo(idVideo)
+    setOpenModalFiles(true);
+  };
+
+  const getFilesVideo = async (video:string) => {
+    const result = await services.getFiles(video);
+    if(result?.data?.payload) {
+      setListFiles(result.data.payload)
+    }
+  };
+
+  const deleteFile = async (file: string, index: number) => {
+    console.log("index:" , index)
+    console.log("file:" , file)
+    const result = await services.deleteFile(currentVideoSelected, file);
+
+    if(result?.data?.payload) {
+      setListFiles(result.data.payload)
+      messageApi.open({
+        type: "success",
+        content: "Se realizó la acción de forma correcta.",
+      });
+    }
+  };
+
   const handleCancel = () => {
     setOpen(false);
   };
+
+  const handleCancelModalFiles = () => {
+    setOpenModalFiles(false);
+  };
+
+  const propsFiles: UploadProps = {
+    name: "file",
+    action: `${base_url}/videos/${currentVideoSelected}/uploadFile`,
+    headers: {},
+    onChange(info) {
+      if (info.file.status !== "uploading") {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === "done") {
+        console.log("info file: ", info)
+        const responseFile = info.file.response.payload;
+        setListFiles([...responseFile]);
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === "error") {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+  };
+
   const EditableText = ({ func, text, index }: any) => {
     return (
       <Typography.Title
         editable={{
           onChange: async (value) => {
-            await func(value)
-          }
+            await func(value);
+          },
         }}
         level={4}
         style={{ margin: 0 }}
@@ -182,98 +246,157 @@ export const VideoEditor = (props:customProps) => {
       >
         {text}
       </Typography.Title>
-    )
-  }
+    );
+  };
 
   return (
     <>
       <ConfigProvider
         theme={{
-          algorithm: darkAlgorithm
+          algorithm: darkAlgorithm,
         }}
       >
         {contextHolder}
-        <Space style={{ display: 'flex', flexWrap: "wrap", maxWidth: '1000px', gap: '20px', marginTop: '30px' }}>
-          {
-            props.videos.map((value: any, index: any) => {
-              return (
-                <Card
-                  key={index}
-                  title={
-                    <EditableText text={value['name']} func={
-                      (text: any) => {
-                        editVideoInfo(value['id'], { 'name': text })
-                      }
-                    } />
-                  }
-                  style={{ width: 300 }}
-                  extra={
-                    <Popover content={<p>Preguntas</p>}>
-                      <PlusCircleOutlined style={{ color: 'green', cursor: 'pointer' }} onClick={() => {  openModal(value['id'], index) }} />
-                    </Popover>
-
-                  }
-                >
-                  <img
-                    src={value['thumbnail']}
-                    style={{ width: '100%' }}
+        <Space
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            maxWidth: "1000px",
+            gap: "20px",
+            marginTop: "30px",
+          }}
+        >
+          {props.videos.map((value: any, index: any) => {
+            return (
+              <Card
+                key={index}
+                title={
+                  <EditableText
+                    text={value["name"]}
+                    func={(text: any) => {
+                      editVideoInfo(value["id"], { name: text });
+                    }}
                   />
+                }
+                style={{ width: 300 }}
+                // extra={
+                //   <Popover content={<p>Preguntas</p>}>
+                //     <Button type="primary"  onClick={() => {  openModal(value['id'], index) }}>
+                //       Preguntas
+                //     </Button>
+                //     <PlusCircleOutlined style={{ color: 'green', cursor: 'pointer' }} onClick={() => {  openModal(value['id'], index) }} />
+                //   </Popover>
 
-                  <span style={{ fontSize: 18 }}>
-                    <EditableText text={value['description']} func={
-                      (text: any) => {
-                        editVideoInfo(value['id'], { 'description': text })
-                      }
-                    } />
-                  </span>
-                </Card>
-              )
-            })
-          }
+                // }
+
+                actions={[
+                  <QuestionOutlined
+                    key="questions"
+                    onClick={() => {
+                      openModal(value["id"], index);
+                    }}
+                  />,
+                  <FilePdfOutlined
+                    key="files"
+                    onClick={() => {
+                      handleOpenModalFiles(value["id"], index);
+                    }}
+                  />,
+                ]}
+              >
+                <img src={value["thumbnail"]} style={{ width: "100%" }} />
+
+                <span style={{ fontSize: 18 }}>
+                  <EditableText
+                    text={value["description"]}
+                    func={(text: any) => {
+                      editVideoInfo(value["id"], { description: text });
+                    }}
+                  />
+                </span>
+              </Card>
+            );
+          })}
         </Space>
 
-          <NewVideo id = {props.id} numberofVideos = {props.videos.length || 0} />
+        <NewVideo id={props.id} numberofVideos={props.videos.length || 0} />
 
-        <Modal width="80vw" title="Preguntas" open={open} onCancel={handleCancel} onOk={() => { updateQuestions() }} >
-          <div style={{ display: 'flex', gap: 50, width: '100%', justifyContent: 'center' }}>
-          {questions.length > 0 &&
-                    questions.map((item, index: any) => {
-                      return (
-
-                        <Row>
-                        <Col>
-                          <Card key={index} title={<EditableText text={item.question}
-                          func={
-                            (text: any) => {
-                              editQuestionTitle(text, index)
-                            }
+        <Modal
+          width="80vw"
+          title="Preguntas"
+          open={open}
+          onCancel={handleCancel}
+          onOk={() => {
+            updateQuestions();
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              gap: 50,
+              width: "100%",
+              justifyContent: "center",
+            }}
+          >
+            {questions.length > 0 &&
+              questions.map((item, index: any) => {
+                return (
+                  <Row>
+                    <Col>
+                      <Card
+                        key={index}
+                        title={
+                          <EditableText
+                            text={item.question}
+                            func={(text: any) => {
+                              editQuestionTitle(text, index);
+                            }}
+                            ind={index}
+                          />
+                        }
+                        extra={
+                          <DeleteOutlined
+                            style={{
+                              color: "red",
+                              fontSize: 18,
+                              marginLeft: 100,
+                            }}
+                          />
+                        }
+                      >
+                        <Radio.Group
+                          value={valueOptionSelected(item.options)}
+                          onChange={(e) =>
+                            editCorrectOptionQuestion(index, e.target.value)
                           }
-                          ind = {index}
-                          />} extra={<DeleteOutlined style={{ color: 'red', fontSize: 18, marginLeft: 100 }} />}>
-                            <Radio.Group value={valueOptionSelected(item.options)}  onChange={(e) => editCorrectOptionQuestion(index, e.target.value)}>
-                              <Space direction="vertical">
-                                {
-                                  item.options.map((option, indexOption: any) => {
-                                    return (
-                                      <Radio key={indexOption} value={indexOption}>{<EditableText text={option.option}
-                                      func={
-                                        (text: any) => {
-                                          editOptionQuestion(text, index, indexOption)
-                                        }
-                                      }
-                                      />}</Radio>
-                                    )
-                                  })
-                                }
-                              </Space>
-                            </Radio.Group>
-                          </Card>
-                        </Col>
-                      </Row>
+                        >
+                          <Space direction="vertical">
+                            {item.options.map((option, indexOption: any) => {
+                              return (
+                                <Radio key={indexOption} value={indexOption}>
+                                  {
+                                    <EditableText
+                                      text={option.option}
+                                      func={(text: any) => {
+                                        editOptionQuestion(
+                                          text,
+                                          index,
+                                          indexOption
+                                        );
+                                      }}
+                                    />
+                                  }
+                                </Radio>
+                              );
+                            })}
+                          </Space>
+                        </Radio.Group>
+                      </Card>
+                    </Col>
+                  </Row>
+                );
+              })}
 
-                      )
-                    })}
-          
             {/* <Row>
               <Col>
                 <Card title={<EditableText text={'QUESTION 1'} />} extra={<DeleteOutlined style={{ color: 'red', fontSize: 18, marginLeft: 100 }} />}>
@@ -318,13 +441,51 @@ export const VideoEditor = (props:customProps) => {
                 </Card>
               </Col>
             </Row> */}
-
           </div>
-
-
         </Modal>
 
+        <Modal
+          width="80vw"
+          title="Archivos"
+          open={openModalFiles}
+          onCancel={handleCancelModalFiles}
+          onOk={() => {
+            updateQuestions();
+          }}
+        >
+    
+            <Row>
+              <Col  xs={12}>
+                <Upload {...propsFiles}>
+                  <Button icon={<UploadOutlined />}>Click to Upload</Button>
+                </Upload>
+              </Col>
+            </Row>
 
+            <Row>
+              <Col  xxl={24}>
+                <List
+                  className="demo-loadmore-list"
+                  itemLayout="horizontal"
+                  dataSource={listFiles}
+                  renderItem={(item:any, index) => (
+                    <List.Item
+                      actions={[
+                        <Button onClick={() => deleteFile(item.id, index)} icon={<DeleteOutlined />}>Eliminar</Button>
+                      ]}
+                    >
+
+                    <List.Item.Meta
+                      title={item?.name}
+                    />
+
+                    </List.Item>
+                    
+                  )}
+                />
+              </Col>
+            </Row>
+        </Modal>
       </ConfigProvider>
     </>
   );
--- src/services/http.tsx
+++ src/services/http.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+const base_url = import.meta.env.VITE_BASE_URL
 const post = async (url: string) => {
   const result = await axios({
     method: "get",
@@ -11,7 +11,7 @@ const post = async (url: string) => {
 
 const getCourses = async () => {
   const courses_res = await axios.get(
-    "https://nestjs-virgo-production.up.railway.app/courses",
+    base_url + "/courses",
     {
       headers: {
         "Access-Control-Allow-Origin": true,
@@ -25,7 +25,7 @@ const getCourses = async () => {
 const postScore = async (id: string, score: number) => {
   const result = await axios({
     method: "post",
-    url: `https://nestjs-virgo-production.up.railway.app/videos/${id}/score`,
+    url: `${base_url}/videos/${id}/score`,
     data: {
       score,
     },
@@ -36,7 +36,7 @@ const postScore = async (id: string, score: number) => {
 const getCourseVideos = async (courseId: String) => {
   try {
     const res = await axios.get(
-      `https://nestjs-virgo-production.up.railway.app/courses/${courseId}`,
+      `${base_url}/courses/${courseId}`,
       {
         headers: {
           "Access-Control-Allow-Origin": true,
@@ -51,7 +51,7 @@ const getCourseVideos = async (courseId: String) => {
 
 const getUserInfo = async (email: String) => {
   const res = await axios.get(
-    `https://nestjs-virgo-production.up.railway.app/user/${email}`
+    `${base_url}/user/${email}`
   );
   return res.data.payload;
 };
@@ -61,7 +61,7 @@ const updateCourse = async (id: string, field: string, data: any) => {
   body[field] = data;
   console.log(body);
   const res = await axios.put(
-    `https://nestjs-virgo-production.up.railway.app/courses/${id}`,
+    `${base_url}/courses/${id}`,
     body
   );
   return res;
@@ -69,13 +69,13 @@ const updateCourse = async (id: string, field: string, data: any) => {
 
 const getInfo = async (id: string | undefined) => {
   let category: any = await axios.get(
-    `https://nestjs-virgo-production.up.railway.app/category`
+    `${base_url}/category`
   );
   let routes: any = await axios.get(
-    `https://nestjs-virgo-production.up.railway.app/route`
+    `${base_url}/route`
   );
   let course: any = await axios.get(
-    `https://nestjs-virgo-production.up.railway.app/courses/${id}`
+    `${base_url}/courses/${id}`
   );
   let r_ = [];
   let c_ = [];
@@ -107,7 +107,7 @@ const getInfo = async (id: string | undefined) => {
 
 const editVideoInfo = async (id: string, body: any) => {
   const res = await axios.post(
-    `https://nestjs-virgo-production.up.railway.app/videos/${id}`,
+    `${base_url}/videos/${id}`,
     body
   );
   window.location.reload();
@@ -116,7 +116,7 @@ const editVideoInfo = async (id: string, body: any) => {
 
 const editCourseCover = async (id: string, body: any) => {
   const res = await axios.post(
-    `https://nestjs-virgo-production.up.railway.app/courses/${id}/uploadCover`,
+    `${base_url}/courses/${id}/uploadCover`,
     body
   );
   window.location.reload();
@@ -124,7 +124,7 @@ const editCourseCover = async (id: string, body: any) => {
 
 const newCourse = async (name: string) => {
   const res = await axios.post(
-    `https://nestjs-virgo-production.up.railway.app/courses`,
+    `${base_url}/courses`,
     {
       name: name,
     }
@@ -134,7 +134,7 @@ const newCourse = async (name: string) => {
 
 const newVideo = async (id: string, body: any) => {
   const res = await axios.post(
-    `https://nestjs-virgo-production.up.railway.app/courses/${id}/uploadVideo`,
+    `${base_url}/courses/${id}/uploadVideo`,
     body,
     { timeout: 1800000, headers: { "Access-Control-Allow-Origin": "*" } }
   );
@@ -143,7 +143,7 @@ const newVideo = async (id: string, body: any) => {
 
 const getRoutes = async () => {
   const res = await axios.get(
-    "https://nestjs-virgo-production.up.railway.app/route"
+    "${base_url}/route"
   );
   return res;
 };
@@ -152,7 +152,7 @@ const editUserVideoProgress = async (userId: String, body: any) => {
   console.log(userId, body);
   try {
     const res = await axios.post(
-      `https://nestjs-virgo-production.up.railway.app/user/${userId}/course/inProgress`,
+      `${base_url}/user/${userId}/course/inProgress`,
       body
     );
     console.log(res);
@@ -165,7 +165,7 @@ const editUserVideoProgress = async (userId: String, body: any) => {
 
 const getCertificate = async (body: any) => {
   const res = await axios.post(
-    `https://nestjs-virgo-production.up.railway.app/user/courseCertificate`,
+    `${base_url}/user/courseCertificate`,
     body
   );
   const base64String = res.data.toString("base64");
@@ -179,14 +179,14 @@ const getCertificate = async (body: any) => {
 
 const getStatistics = async (idUser: any) => {
   const res = await axios.get(
-    `https://nestjs-virgo-production.up.railway.app/user/${idUser}/progressInfo`
+    `${base_url}/user/${idUser}/progressInfo`
   );
   return res;
 };
 
 const getQuestions = async (idVideo: string) => {
   const res = await axios.get(
-    `https://nestjs-virgo-production.up.railway.app/videos/${idVideo}/questions`
+    `${base_url}/videos/${idVideo}/questions`
   );
   return res;
 }
@@ -194,12 +194,25 @@ const getQuestions = async (idVideo: string) => {
 const addQuestions = async (idVideo: string, body: any) => {
   const bodyQuestion = { questions: body}
   const res = await axios.post(
-    `https://nestjs-virgo-production.up.railway.app/videos/${idVideo}/questions`,
+    `${base_url}/videos/${idVideo}/questions`,
     bodyQuestion
   );  
   return res;
 }
 
+const getFiles = async (idVideo: string) => {
+  const res = await axios.get(
+    `${base_url}/videos/${idVideo}/files`
+  );
+  return res;
+}
+
+const deleteFile = async (idVideo: string, file: string) => {
+  const res = await axios.delete(
+    `${base_url}/videos/${idVideo}/file/${file}`
+  );
+  return res;
+}
 export default {
   post,
   getCourses,
@@ -218,4 +231,6 @@ export default {
   getStatistics,
   getQuestions,
   addQuestions,
+  getFiles,
+  deleteFile,
 };
