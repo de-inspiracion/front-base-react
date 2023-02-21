de06531993588aa10ccfa9a52de8b590ffc7bfe0
Philtaboada <philtaboada1997@gmail.com>
2023-02-21 00:25:08 -0500

    arreglo de directiva

--- src/pages/directive/pages/DirectiveMain.tsx
+++ src/pages/directive/pages/DirectiveMain.tsx
@@ -1,78 +1,84 @@
-import { Button, Space, Table } from 'antd'
-import React, { useEffect, useState } from 'react'
-import Column from 'antd/es/table/Column';
-import axios from 'axios';
+import { Button, Space, Table } from "antd";
+import React, { useEffect, useState } from "react";
+import Column from "antd/es/table/Column";
+import axios from "axios";
 import services from "../../../services/http";
-
+import { useSelector } from "react-redux";
 
 interface UsersInfo {
-    _id: string;
-    name: string;
-    enable: boolean;
-    directive: string;
+  id: string;
+  name: string;
+  enable: boolean;
+  directive: string;
 }
 
 const DirectorMain: React.FC = () => {
-    const [isActive, setIsActive] = useState<boolean>(false);
-    const [idUser, setIdUser] = useState<string>('');
-    const [stated, setStated] = useState<boolean>(true)
-    const [usersInfo, setUsersInfo] = useState<UsersInfo[]>([])
+  const [isActive, setIsActive] = useState<boolean>(false);
+  const [idUser, setIdUser] = useState<string>("");
+  const [stated, setStated] = useState<boolean>(true);
+  const [usersInfo, setUsersInfo] = useState<UsersInfo[]>([]);
 
-    useEffect(() => {
-        const getUsers = async () => {
-          // setIems(await services.getCourseVideos(courseData[0].id).payload)
-          let res = await services.getUsers();
-          setUsersInfo(res);
-        };
-        getUsers();
-      }, []);
+  const userInfo = useSelector((estado: any) => estado.userInfo);
 
-    console.log(usersInfo)
-    // Desactivar solo uno
-    const toggleOnebyOne = (id: string) => {
-        const newData = [...usersInfo];
-        const target = newData.find((item) => id === item._id);
-        if (target) {
-            target.enable = !target.enable;
-            setIsActive(!isActive);
-            setStated(target.enable)
-            setIdUser(target._id)
-        }
+  useEffect(() => {
+    const usersDirective = async (idDirective: string) => {
+      const res = await services.usersDirective(idDirective);
+      setUsersInfo(res.data);
     };
-    // postear a la base de datos
-    const postToDB = () => {
-        const IDuser = idUser
-        const state = stated
-        axios.post(`https://nestjs-virgo-production.up.railway.app/user/${IDuser}/enable/${state}`)
-            .then(res => {
-                console.log('se envio')
-                console.log(res)
-            })
-            .catch(err => {
-                console.log(err)
-                console.log('no se envio')
-            })
+    usersDirective(userInfo.directive?.id);
+  }, []);
+
+  // Desactivar solo uno
+  const toggleOnebyOne = (id: string) => {
+    const newData = [...usersInfo];
+    const target = newData.find((item) => id === item.id);
+    if (target) {
+      target.enable = !target.enable;
+      setIsActive(!isActive);
+      setStated(target.enable);
+      setIdUser(target.id);
     }
-    useEffect(() => {
-        postToDB()
-    }, [idUser])
+  };
 
-    return (
-        <>
-    <Table dataSource={usersInfo}>
+  //   postear a la base de datos
+  const postToDB = () => {
+    const IDuser = idUser;
+    const state = stated;
+    axios
+      .post(
+        `https://nestjs-virgo-production.up.railway.app/user/${IDuser}/enable/${state}`
+      )
+      .then((res) => {
+        console.log("se envio");
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log("no se envio");
+      });
+  };
+  useEffect(() => {
+    postToDB();
+  }, [idUser]);
+
+  return (
+    <>
+      <Table dataSource={usersInfo}>
         <Column title="Name" dataIndex="name" key="name" />
-        <Column title="Colegio" dataIndex="directive" key="firstName" />
         <Column
-      title="Action"
-      key="action"
-      render={(user: any) => (
-        <Space size="middle">
-            <Button onClick={() => toggleOnebyOne(user._id)}>{user.enable ? 'Desabilitar' : 'Habilitar'}</Button>
-        </Space>
-      )}
-    />
-    </Table>
+          title="Action"
+          key="action"
+          render={(user: any) => (
+            <Space size="middle">
+              <Button onClick={() => toggleOnebyOne(user.id)}>
+                {user.enable ? "Desabilitar" : "Habilitar"}
+              </Button>
+            </Space>
+          )}
+        />
+      </Table>
     </>
-)}
+  );
+};
 
-export default DirectorMain
+export default DirectorMain;
